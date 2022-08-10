d3d5e51f48fa7738e51f3ce87b3ba80287a5efdc
JordanPlayz158 <JordanAdamsBusiness@outlook.com>
2022-08-10 00:15:29 -0400

    Updated REQUEST_URL in UpdateChecker.java as I have now made the resource

--- src/main/java/cl/bebt/staffcore/utils/UpdateChecker.java
+++ src/main/java/cl/bebt/staffcore/utils/UpdateChecker.java
@@ -5,20 +5,12 @@ import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 import org.bukkit.Bukkit;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.nio.charset.StandardCharsets;
 import java.util.function.Consumer;
-import java.util.stream.Collectors;
 
 public class UpdateChecker {
     private static final Gson GSON = new Gson();
     private static String USER_AGENT  = "Staff-Core";
-    private static final String REQUEST_URL = "https://api.spiget.org/v2/resources/82324/versions/latest";
+    private static final String REQUEST_URL = "https://api.spiget.org/v2/resources/104420/versions/latest";
 
     private final StaffCorePlugin plugin;
 
